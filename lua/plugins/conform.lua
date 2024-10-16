f4e29b6782b390da2a83a3eb78ab6b535ff62aca
Frank Dugan III <frank3@duganator.com>
2024-10-16 12:59:47 -0500

    specify HTML formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -22,7 +22,8 @@ return {
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      javascript = { { 'prettierd', 'prettier' } },
+      javascript = { 'prettierd', 'prettier' },
+      html = { 'prettierd', 'prettier' },
     },
   },
 }
