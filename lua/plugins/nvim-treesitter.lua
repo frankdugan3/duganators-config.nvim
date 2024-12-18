acd71d9b9a71c6a4ca9c9b4d7ba4bc36532a53c0
Frank Dugan III <frank3@duganator.com>
2024-12-17 19:51:44 -0600

    add incremental select support

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,47 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  event = { 'BufReadPre', 'BufNewFile' },
   opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'elixir', 'heex', 'eex', 'go', 'zig', 'rust', 'toml' },
+    ensure_installed = {
+      'bash',
+      'c',
+      'css',
+      'diff',
+      'dockerfile',
+      'eex',
+      'elixir',
+      'gitignore',
+      'go',
+      'heex',
+      'html',
+      'json',
+      'lua',
+      'luadoc',
+      'markdown',
+      'rust',
+      'toml',
+      -- 'typsecript',
+      'vim',
+      'vimdoc',
+      'yaml',
+      'zig',
+    },
     auto_install = true,
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = { 'ruby' },
     },
     indent = { enable = true, disable = { 'ruby' } },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<C-space>',
+        node_incremental = '<C-space>',
+        scope_incremental = false,
+        node_decremental = '<bs>',
+      },
+    },
   },
   config = function(_, opts)
     require('nvim-treesitter.install').prefer_git = true
