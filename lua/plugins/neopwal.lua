95202b62e1521fdd99596f3c71405c816f361856
Frank Dugan III <frank3@duganator.com>
2025-02-18 15:39:56 -0600

    tweak theme

--- lua/plugins/neopwal.lua
+++ lua/plugins/neopwal.lua
@@ -5,7 +5,22 @@ return {
   priority = 1000,
   opts = {
     use_palette = 'wallust',
+    terminal_colors = true,
     transparent_background = true,
+    styles = {
+      comments = { 'italic' },
+      conditionals = {},
+      loops = {},
+      functions = {},
+      keywords = {},
+      includes = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      types = {},
+      operators = {},
+    },
   },
   init = function()
     vim.cmd.colorscheme 'neopywal-dark'
