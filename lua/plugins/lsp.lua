db1fe98bd61e8e1928442abed2af0853bb23b70e
Frank Dugan III <frank3@duganator.com>
2025-04-01 10:14:54 -0500

    Fix mason setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -185,6 +185,8 @@ return {
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
+      ensure_installed = {},
+      automatic_installation = false,
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
