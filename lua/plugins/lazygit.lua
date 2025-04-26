936f735fbc218a3a968e3a3962de2f4d0c189238
Frank Dugan III <frank3@duganator.com>
2025-04-26 12:01:06 -0500

    More consistent keybindings

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -13,8 +13,8 @@ return {
   },
   keys = {
     { '<leader>gl', '<cmd>LazyGit<cr>', desc = '[L]azyGit' },
-    { '<leader>gf', '<cmd>LazyGitCurrentFile<cr>', desc = 'LazyGit Current [F]ile' },
     { '<leader>gr', '<cmd>LazyGitFilter<cr>', desc = 'LazyGit [R]eflog' },
-    { '<leader>gb', '<cmd>LazyGitFilterCurrentFile<cr>', desc = 'LazyGit [B]uffer Reflog' },
+    { '<leader>gf', '<cmd>LazyGitCurrentFile<cr>', desc = 'LazyGit Current [F]ile' },
+    { '<leader>gb', '<cmd>LazyGitFilterCurrentFile<cr>', desc = 'LazyGit Current File Reflog' },
   },
 }
