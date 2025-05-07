5bb244d5f63f63a156214b7402aeb551ba63323c
Frank Dugan III <frank3@duganator.com>
2025-05-07 12:18:04 -0500

    Add custom sigils for Pyro support

---
+++ queries/elixir/injections.scm
@@ -0,0 +1,12 @@
+;; extends
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content
+  (#any-of? @_sigil_name "JS")
+  (#set! injection.language "javascript"))
+
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content
+  (#any-of? @_sigil_name "CSS")
+  (#set! injection.language "css"))
