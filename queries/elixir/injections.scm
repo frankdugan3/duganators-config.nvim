f16f1a2a2ca0e689924018da13a04f4ecf195bf9
Frank Dugan III <frank3@duganator.com>
2025-06-23 10:54:23 -0500

    Add custom sigil for SQL

--- queries/elixir/injections.scm
+++ queries/elixir/injections.scm
@@ -10,3 +10,9 @@
   (quoted_content) @injection.content
   (#any-of? @_sigil_name "CSS")
   (#set! injection.language "css"))
+
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content
+  (#any-of? @_sigil_name "SQL")
+  (#set! injection.language "sql"))
