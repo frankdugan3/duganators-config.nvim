90d5fe091e0bb3e232bee4a3b9d9f3976a297544
Frank Polasek Dugan III <frank3@duganator.com>
2026-02-26 22:41:51 -0600

    Add ~TYPST

--- queries/elixir/injections.scm
+++ queries/elixir/injections.scm
@@ -16,3 +16,9 @@
   (quoted_content) @injection.content
   (#any-of? @_sigil_name "SQL")
   (#set! injection.language "sql"))
+
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content
+  (#any-of? @_sigil_name "TYPST")
+  (#set! injection.language "typst"))
